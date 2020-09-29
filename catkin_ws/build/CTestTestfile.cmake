37556b1c0970da0e65e9a521f6f3e0082a9a8705
xhc-hit <xhc_hit@163.com>
2020-09-29 12:08:18 +0800

    frist ros commit

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/xhc/workspace/ros-program/catkin_ws/src
+# Build directory: /home/xhc/workspace/ros-program/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(beginner_tutorials)
